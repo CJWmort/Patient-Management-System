9e0585c15d1ba90c2b2d998c2fb7dd9ca7a3604a
CJWmort <cjwmort@gmail.com>
2022-05-09 14:41:10 +0800

    rearranging files

---
+++ thkh-backend/resources/views/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THKH - Login</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+    <div class="login_div">
+        <form action="{{route('login-user')}}" method="POST">
+        {{ csrf_field() }}
+            <div>
+                <p><img class="loginlogo" src="../images/thkh-logo.jpg" alt="logo"></p>
+                <p class="logintitle">LOG IN</p>
+                <p><input class="loginID" name="login_id" type="text" placeholder="Login ID" required></p>
+                <p><input class="password" name="password" type="password" placeholder="Password" required></p>
+                <p class="remember">
+                    <input class="logincheck" type="checkbox"><label>Remember Password</label>
+                    @if($errors->any())
+                        <b>{{$errors->first()}}</b>
+                    @endif
+                </p>
+                <p class="forgot"><a href="#">Forgot Password  ?</a></p>
+                <p><input class="login_button" name="login" type="submit" value="Log In"></p>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
--- thkh-backend/routes/api.php
+++ thkh-backend/routes/api.php
@@ -1,17 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::get('/', function(){
     return view('welcome');
 });
@@ -20,8 +27,3 @@ Route::get('/login', [UserController::class, 'login'])->middleware('alreadyLogge
 Route::post('/login-user', [UserController::class, 'loginUser'])->name('login-user');
 Route::get('/main', [UserController::class, 'main'])->middleware('isLoggedIn');
 Route::get('/logout', [UserController::class, 'logout']);
-
-
-
-
-

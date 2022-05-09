b85a114495609ea4df819c7bd6ecc28fa556a86e
CJWmort <cjwmort@gmail.com>
2022-05-09 15:28:30 +0800

    rearranging files

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\UserController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::get("login", [UserController::class, 'login']);
 Route::get('/', function(){
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/login', [UserController::class, 'login'])->middleware('alreadyLoggedIn');

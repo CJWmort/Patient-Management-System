ff5dea22611bf049c0e33ab9127b6f55663a17ba
zulhiillmi <201809N@mymail.nyp.edu.sg>
2022-05-30 10:08:26 +0800

    creating select queries to display in chart

--- app/Http/Controllers/Chart8Controller.php
+++ app/Http/Controllers/Chart8Controller.php
@@ -19,6 +19,45 @@ class Chart8Controller extends Controller
         if (Session::has('loginId')){
             $data = User::where('id', '=', Session::get('loginId'))->first();
         }
-        return view('chart',compact('data', 'selectedChart'));
+
+        //Store serious reported event for each month
+        $datesSeriousFall = DB::select('');
+
+        foreach($datesSeriousFall as $row){
+            $date_x_axis[] = $row->a_inccidentDate;
+            $srefall_y_axis[] = $row->sre_num;
+        }
+
+        $formattedDate = array_map(function ($date) {
+            return date('M-y', strtotime($date));
+        }, $date_x_axis);
+
+        $chartData = array_combine($formattedDate, $srefall_y_axis);
+
+        $listSREFall = array();
+        for($i = 0; $i<sizeof($chartData); $i++) {
+            array_push($listSREFall, (Object)["x" => $formattedDate[$i], "y" => $srefall_y_axis[$i]]);
+        }
+
+        //Store serious reported event for each month
+        $datesSeriousMedicine = DB::select('');
+
+        foreach($datesSeriousMedicine as $row){
+            $date_x_axis[] = $row->a_inccidentDate;
+            $sremedicine_y_axis[] = $row->sre_num;
+        }
+
+        $formattedDate = array_map(function ($date) {
+            return date('M-y', strtotime($date));
+        }, $date_x_axis);
+
+        $chartData = array_combine($formattedDate, $sremedicine_y_axis);
+
+        $listSREMedicine = array();
+        for($i = 0; $i<sizeof($chartData); $i++) {
+            array_push($listSREMedicine, (Object)["x" => $formattedDate[$i], "y" => $sremedicine_y_axis[$i]]);
+        }
+        
+        return view('chart',compact('data', 'listSREFall', 'listSREMedicine', 'selectedChart'));
     }
 }

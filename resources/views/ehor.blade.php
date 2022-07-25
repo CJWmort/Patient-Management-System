9f93faf1c232a54460edf41a24c886c3471f88c8
CJWmort <cjwmort@gmail.com>
2022-07-25 09:03:57 +0800

    remove old code comments in EHOR page

--- resources/views/ehor.blade.php
+++ resources/views/ehor.blade.php
@@ -33,6 +33,7 @@
             onclick="window.location='{{ route('type') }}'" id="typebtn" class="toggleBtn">Type of Occurrence
         </button> 
     </div>
+    
     @if($selectedOccurrence == 'location')
     <!-- Location of Occurrence fields Table -->
     <div id="displaytable_location">
@@ -64,30 +65,7 @@
         </table>
     </div>
     @endif
-    <!-- location end -->
-
-    <!-- <form class="location-title site" action="{{route('addsite')}}" method="POST">
-    @csrf
-        <span class="addfield">
-            Site of Occurrence
-            <button class="addBtn" type="submit"><img src="{{URL::asset('public/images/add.png')}}" alt="add"></button>
-            <input class="newlocation" type="text" name="site" placeholder="enter new site" required>  
-        </span> 
-        <span class="alert-msg">{{ session()->get('msg2') }}</span>
-    </form>
-    <form class="location" action="{{route('updatesite')}}" method="POST">
-    @csrf
-        <div class="location-grid">
-            @foreach($allSites as $sites)
-            <div>
-                <a class="deleteBtn" href="{{route('deletesite', ['id'=>$sites->id])}}"><img src="{{URL::asset('public/images/minus.png')}}" onclick="return confirm('Are you sure that you want to delete the field {{$sites->site}} ?')" alt="delete"></a><input type="text" name="site[]" value="{{$sites->site}}" required>
-                <input type="hidden" name="siteid[]" value="{{$sites->id}}">                 
-            </div>
-            @endforeach
-        </div>
-        <input class="update" type="submit" value="Update Site of Occurrence Fields">
-    </form> -->
-
+   
     <!-- Site of Occurrence fields Table -->
     @if($selectedOccurrence == 'site')
     <div id="displaytable_site">
@@ -184,153 +162,14 @@
         <br><br><br>
     </div>
     @endif
-    <script src="{{URL::asset('public/js/jquery.min.js?v=').time()}}"></script>
-    <script> //Highlight current occurrence that the admin is viewing
-        @if($selectedOccurrence == 'type')
-            $('#typebtn').css({backgroundColor: '#0D6E6B', color: 'whitesmoke'})
-        @elseif($selectedOccurrence == 'site')
-            $('#sitebtn').css({backgroundColor: '#0D6E6B', color: 'whitesmoke'})
-        @elseif($selectedOccurrence == 'location')
-            $('#locationbtn').css({backgroundColor: '#0D6E6B', color: 'whitesmoke'})
-        @endif
-    </script>
-
-
-
-
-    <!-- <div class="ehortitle">EDIT EHOR FIELDS</div>
-    Form for location of occurrence
-    <form class="location-title" action="{{route('addlocation')}}" method="POST">
-    @csrf
-        <span class="addfield">
-            Location of Occurrence
-            <button class="addBtn" type="submit"><img src="{{URL::asset('public/images/add.png')}}" alt="add"></button>
-            <input class="newlocation" type="text" name="location" placeholder="enter new location" required>  
-        </span> 
-        <span class="alert-msg">{{ session()->get('msg') }}</span>
-    </form>
-    <form class="location" action="{{route('updatelocation')}}" method="POST">
-    @csrf
-        <div class="location-grid">
-            @foreach($allLocations as $locations)
-            <div>
-                <a class="deleteBtn" href="{{route('deletelocation', ['id'=>$locations->id])}}"><img src="{{URL::asset('public/images/minus.png')}}" onclick="return confirm('Are you sure that you want to delete the field {{$locations->location}} ?')" alt="delete"></a><input type="text" name="location[]" value="{{$locations->location}}" required>
-                <input type="hidden" name="locationid[]" value="{{$locations->id}}">                 
-            </div>
-            @endforeach
-        </div>
-        <input class="update" type="submit" value="Update Location of Occurrence Fields">
-    </form> -->
-
-    
-    <!-- Form for site of occurrence -->
-    <!-- <form class="location-title site" action="{{route('addsite')}}" method="POST">
-    @csrf
-        <span class="addfield">
-            Site of Occurrence
-            <button class="addBtn" type="submit"><img src="{{URL::asset('public/images/add.png')}}" alt="add"></button>
-            <input class="newlocation" type="text" name="site" placeholder="enter new site" required>  
-        </span> 
-        <span class="alert-msg">{{ session()->get('msg2') }}</span>
-    </form>
-    <form class="location" action="{{route('updatesite')}}" method="POST">
-    @csrf
-        <div class="location-grid">
-            @foreach($allSites as $sites)
-            <div>
-                <a class="deleteBtn" href="{{route('deletesite', ['id'=>$sites->id])}}"><img src="{{URL::asset('public/images/minus.png')}}" onclick="return confirm('Are you sure that you want to delete the field {{$sites->site}} ?')" alt="delete"></a><input type="text" name="site[]" value="{{$sites->site}}" required>
-                <input type="hidden" name="siteid[]" value="{{$sites->id}}">                 
-            </div>
-            @endforeach
-        </div>
-        <input class="update" type="submit" value="Update Site of Occurrence Fields">
-    </form> -->
-
-    
-
-
-
-
-
-
-    
-
-<!-- NEW ONE -->
-<!-- <form class="location-title type" action="{{route('addlocation')}}" method="POST">
-    @csrf
-        <span class="addfield">
-            Location of Occurrence
-            <button class="addBtn" type="submit"><img src="{{URL::asset('public/images/add.png')}}" alt="add"></button>
-            <input class="newlocation" type="text" name="location" placeholder="enter new location" required>  
-        </span> 
-        <span class="alert-msg">{{ session()->get('msg') }}</span>
-    </form>
-    <form class="location" action="{{route('updatelocation')}}" method="POST">
-    @csrf
-        <div class="location-grid2">
-                @foreach($allLocations as $locations)
-                <tr class="tablerow">
-                    <td><input type="text" name="name[]" value="{{$locations->location}}" required></td>
-                    <td><a href="" class="update">Update</a></td>
-                    <td><a href="{{route('deletelocation', ['id'=>$locations->id])}}" class="delete" onclick="return confirm('Are you sure that you want to delete the field {{$locations->location}} ?')" alt="delete">Delete</a></td>
-                </tr>
-                @endforeach
-        </div>
-    </form> -->
-
-    <!--End-->
-
-
-
-
-    <!-- Form for type of occurrence -->
-    <!-- <form class="location-title type" action="{{route('addtype')}}" method="POST">
-    @csrf
-        <span class="addfield">
-            Type of Occurrence
-            <button class="addBtn" type="submit"><img src="{{URL::asset('public/images/add.png')}}" alt="add"></button>
-            <input class="newlocation" type="text" name="name" placeholder="enter new type" required>  
-            <select name="type" required>
-                <option value="" disabled selected>Select Occurrence Type</option>
-                <option value="Fall Related">Fall Related</option>
-                <option value="Medication Related">Medication Related</option>
-                <option value="Other Incidents">Other Incidents</option>
-            </select>
-            <span class="alert-msg">{{ session()->get('msg3') }}</span>
-        </span> 
-    </form> -->
-    <!-- <form class="location-title filter" action="{{route('filtertype')}}" method="POST">
-    @csrf
-        <div class="selectType">
-            <input type="radio" name="type" value="fall">
-            <label>Fall Related</label>
-            <input type="radio" name="type" value="medic">
-            <label>Medication Related</label>
-            <input type="radio" name="type" value="other">
-            <label>Other Incidents</label>
-            <input type="radio" name="type" value="all" checked="checked">
-            <label>All Types</label>
-            <input type="submit" class="filterBtn" value="Filter By Type">
-        </div>
-    </form> -->
-    <!-- <form class="location" action="{{route('updatetype')}}" method="POST">
-    @csrf
-        <div class="location-grid">
-
-            @foreach($allTypes as $types)
-            <tr class="tablerow">
-                <td><input type="text" name="name[]" value="{{$types->name}}" required></td>
-                    <td>
-                        <select name="type" required>
-                            <option {{$types->type == 'Fall Related' ? 'selected' : ''}} value="Fall Related">Fall Related</option>
-                            <option {{$types->type == 'Medication Related' ? 'selected' : ''}} value="Medication Related">Medication Related</option>
-                            <option {{$types->type == 'Other Incidents' ? 'selected' : ''}} value="Other Incidents">Other Incidents</option>
-                        </select>
-                    </td>      
-                <td><a href="" class="update">Update</a></td>
-                <td><a href="{{route('deletetype', ['id'=>$types->id])}}" class="delete" onclick="return confirm('Are you sure that you want to delete the field {{$types->name}} ({{$types->type}}) ?')" alt="delete">Delete</a></td>
-            </tr>
-            @endforeach
-        </table>
-    </div> -->
-    </div>
+</body>
+<script src="{{URL::asset('public/js/jquery.min.js?v=').time()}}"></script>
+<script> //Highlight current occurrence that the admin is viewing
+    @if($selectedOccurrence == 'type')
+        $('#typebtn').css({backgroundColor: '#0D6E6B', color: 'whitesmoke'})
+    @elseif($selectedOccurrence == 'site')
+        $('#sitebtn').css({backgroundColor: '#0D6E6B', color: 'whitesmoke'})
+    @elseif($selectedOccurrence == 'location')
+        $('#locationbtn').css({backgroundColor: '#0D6E6B', color: 'whitesmoke'})
+    @endif
+</script>
